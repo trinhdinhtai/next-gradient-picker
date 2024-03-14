dd5a8762b35b22f916c2316151d92c54171dc651
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-14 23:39:18 +0700

    feat: add icons component and update main navigation

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 
+import { siteConfig } from "@/config/site"
 import MainNav from "@/components/layout/main-nav"
 
 export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav></MainNav>
+        <MainNav items={siteConfig.mainNav} />
       </div>
     </header>
   )
